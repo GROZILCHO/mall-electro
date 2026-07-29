8645a5bf495907c83612591902596be3061d2cfa
Rosen Stanev <rossen.t.stanev@gmail.com>
2026-07-29 13:19:55 +0300

    Fix SSR hydration page preload

--- App.tsx
+++ App.tsx
@@ -217,7 +217,7 @@ export default function App({ ssrPath, ssrPages }: AppProps) {
 
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <AppRoutes ssrPages={ssrPages} />
     </BrowserRouter>
   );
 }
